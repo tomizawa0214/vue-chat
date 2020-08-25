c5a9e8a702ff9eb15f7a6e5695252ba3d95fe287
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-08-25 15:46:04 +0900

    ナビゲーション追加

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,11 +1,11 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyA8TbmrsjakHqK_zorq9zLgai04mhc2np4",
-    authDomain: "vue-chat-c9cef.firebaseapp.com",
-    databaseURL: "https://vue-chat-c9cef.firebaseio.com",
-    projectId: "vue-chat-c9cef",
-    storageBucket: "vue-chat-c9cef.appspot.com",
-    messagingSenderId: "944738237609",
-    appId: "1:944738237609:web:97d47ec2d5c3513071bdb8"
+  apiKey: "AIzaSyA8TbmrsjakHqK_zorq9zLgai04mhc2np4",
+  authDomain: "vue-chat-c9cef.firebaseapp.com",
+  databaseURL: "https://vue-chat-c9cef.firebaseio.com",
+  projectId: "vue-chat-c9cef",
+  storageBucket: "vue-chat-c9cef.appspot.com",
+  messagingSenderId: "944738237609",
+  appId: "1:944738237609:web:97d47ec2d5c3513071bdb8"
 };
 
 export default firebaseConfig;
